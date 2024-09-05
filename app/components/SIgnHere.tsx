e530a2e13f3cb9b932f12e8494dd8479000b9f4c
ShaYri12 <sharjeelb60@gmail.com>
2024-09-05 07:25:05 +0500

    added sign here and done button

--- app/components/SIgnHere.tsx
+++ app/components/SIgnHere.tsx
@@ -1,26 +1,27 @@
 import React from "react";
+import { IoMdPlay } from "react-icons/io";
 
 const SIgnHere = () => {
   return (
-    <div className="col-span-12 mt-2">
+    <div className="col-span-12 mt-2 font-HelveticaNeue px-[12px]">
       <div className="border border-2 border-gray-800 border-l-0 border-r-0 flex items-center justify-between">
         <div className="flex items-center">
           <div
-            className="text-lg font-bold pr-3 w-auto leading-[19px] py-1"
+            className="text-[18px] font-bold font-HelveticaNeue pr-[16px] w-auto leading-[19px] py-1"
             id="scrollToBottom"
           >
             Sign
             <br />
             Here
           </div>
-          <div className="pl-3 font-bold leading-[19px] text-nowrap border-l border-gray-800 py-1">
+          <div className="pl-[16px] font-bold leading-[19px] text-nowrap border-l-2 border-gray-800 py-1">
             Signature of
             <br />
-            U.S. person ▶
+            U.S. person <IoMdPlay className="inline-block" />
           </div>
           <div className="pl-3 w-full md:w-1/2 mr-2 xl:mr-0">
             <button
-              className="bg-gray-300 text-gray-800 px-3 py-1 text-sm font-medium ms-auto"
+              className="bg-[#3097f3] text-white rounded px-[16px] py-1  text-[18px] font-[500] ms-auto"
               data-bs-toggle="modal"
               data-bs-target="#newesign"
               id="esignfocus"
@@ -34,11 +35,14 @@ const SIgnHere = () => {
           </div>
         </div>
         <div className="md:flex items-center">
-          <b>Date ▶</b>
+          <b className="flex items-center">
+            Date
+            <IoMdPlay className="mt-[1px] ms-1" />
+          </b>
           <div className="ml-auto">
             <input
               type="text"
-              className="border-0 bg-white pl-2 text-sm"
+              className="border-0 bg-white text-[16.1px] pl-2 text-sm"
               id="floatingInput"
               placeholder="name"
               disabled
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,16 @@ import SIgnHere from "./components/SIgnHere";
 
 export default function Home() {
   return (
-    <div className="min-w-full p-3 md:block hidden">
+    <div className="min-w-full p-3 md:block hidden pb-[26px]">
       <div className="container-box w-full bg-white mt-12 p-[0.5rem] lg:p-[3rem] pb-4">
         <Header />
         <FormSection />
         <SIgnHere />
+      </div>
+      <div className="text-center mt-[1.5rem] font-HelveticaNeue">
+        <a className="rounded text-[1.15rem] text-white bg-[#f26d21] px-4 py- w-[93.4px] h-[45.2px] flex items-center justify-center font-[500] shadow-md mx-auto inline-block">
+          Done
+        </a>
       </div>
     </div>
   );
