8381c25b524969c1dc99ff6a365ad4c077bfe897
ShaYri12 <sharjeelb60@gmail.com>
2024-09-05 06:45:29 +0500

    form section done

--- app/components/FormSection.tsx
+++ app/components/FormSection.tsx
@@ -5,14 +5,23 @@ import FormThirdSection from "./FormThirdSection";
 
 const FormSection = () => {
   return (
-    <div className="grid grid-cols-12 border-l border-l-black">
-      <FormFirstSection />
-      <div className="col-span-12 font-HelveticaNeue font-[500]">
-        <div className="flex flex-col lg:flex-row">
-          <FormSecondSection />
-        </div>
-        <div className="flex flex-col md:flex-row">
-          <FormThirdSection />
+    <div className="relative">
+      <div className="absolute left-[-110px] top-[40%] text-[16px] font-HelveticaNeue">
+        <p className="rotate-[-90deg] w-fit text-center">
+          <b>Print or type.</b>
+          <br />
+          See <b>Specific Instructions</b> on page 3.
+        </p>
+      </div>
+      <div className="grid grid-cols-12 border-l border-l-black ms-[3rem]">
+        <FormFirstSection />
+        <div className="col-span-12 font-HelveticaNeue font-[500]">
+          <div className="flex flex-col lg:flex-row">
+            <FormSecondSection />
+          </div>
+          <div className="flex flex-col md:flex-row">
+            <FormThirdSection />
+          </div>
         </div>
       </div>
     </div>
